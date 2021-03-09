0b952a7a54d02e247adefc80e18f4783ebc2805e
rodrigos03 <80237085+rodrigos03@users.noreply.github.com>
2021-03-09 14:22:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -22,11 +22,9 @@ function validate2() {
 	insta = $("#insta").val();
 	var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
 	if (twitter === '' && fb === '' && insta === '') {
-		$('#notify2').html('You must enter atleast one social profile email!!');
 		return false;
 	}
 	if ((!filter.test(twitter)) && (!filter.test(fb)) && (!filter.test(fb))) {
-		$('#notify2').html('Enter valid email');
 		return false;
 	}
 
